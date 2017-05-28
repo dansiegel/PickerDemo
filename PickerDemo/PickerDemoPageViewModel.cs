f6e67025d8553a6bf66d4519a02a3720f47cf230
Dan Siegel <me@dansiegel.net>
2017-05-28 09:31:28 -0700

    adding more examples

--- PickerDemo/PickerDemoPageViewModel.cs
+++ PickerDemo/PickerDemoPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace PickerDemo
@@ -7,9 +8,29 @@ namespace PickerDemo
     {
         public PickerDemoPageViewModel()
         {
+            SomeDate = DateTime.Now.AddDays(-7);
+            SomeCollection = new ObservableCollection<string>
+            {
+                "Randolph Carroll",
+                "Orlando Terry",
+                "Gwen Wilson",
+                "Muriel Nguyen",
+                "Gwendolyn Swanson",
+                "Pat Mcdaniel",
+                "Lynn Herrera",
+                "Irene Clark",
+                "Leigh Phillips",
+                "George Maxwell"
+            };
         }
 
         public string SelectedItem { get; set; }
+
+        public string AnotherItem { get; set; }
+
+        public DateTime SomeDate { get; set; }
+
+        public ObservableCollection<string> SomeCollection { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
