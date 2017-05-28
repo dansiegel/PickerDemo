3bacce98f81251452d4939678fff897b0fe323de
Dan Siegel <me@dansiegel.net>
2017-05-27 23:49:28 -0700

    initial commit

---
+++ PickerDemo/PickerDemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PickerDemo
+{
+    public partial class PickerDemoPage : ContentPage
+    {
+        public PickerDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PickerDemo/PickerDemoPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PickerDemo
+{
+    public class PickerDemoPageViewModel : INotifyPropertyChanged
+    {
+        public PickerDemoPageViewModel()
+        {
+        }
+
+        public string SelectedItem { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
